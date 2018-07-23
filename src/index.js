b390d1d62c5bf66d75d945a1788b8bf3526e2ea5
lin jinxing <172601673@qq.com>
2018-07-23 16:03:58 +0800

    state持久化

--- src/index.js
+++ src/index.js
@@ -2,28 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import thunkMiddleware from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
-import reduxer from './reduxers';
-import { fetchLogin } from './actions/identity';
+import reduxers from './reduxers';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { showAlert } from './actions/alert';
-
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reduxers);
+
 const loggerMiddleware = createLogger();
-const store = createStore(
-    reduxer,
+let store = createStore(
+    persistedReducer,
     applyMiddleware(
         loggerMiddleware,
         thunkMiddleware
-    )
+    ),
 );
-
+let persistor = persistStore(store);
 
 ReactDOM.render((
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>
 ), document.getElementById('root'));
 
